7d9af89627f410a9b9370a65060e011175dc51df
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-03-16 12:27:04 +0700

    Fix minor bug related to ownerUsername

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BodyContainer from './BodyContainer'
 
 const App = () => (
-  <BodyContainer ownerUsername="topscores" />
+  <BodyContainer ownerUsername="kaizerwing" />
 )
 
 export default App
